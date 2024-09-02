721e1d40805278daab91a3c1bbd965c7423c4732
moazelgandy2 <zezogameing1234@gmail.com>
2024-09-02 19:15:05 +0300

    chore: Add @nestjs/config dependency and configure environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,15 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { NotesModule } from './notes/notes.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, MongooseModule.forRoot('mongodb://localhost/nest'), NotesModule],
+  imports: [
+    ConfigModule.forRoot(),
+    AuthModule,
+    MongooseModule.forRoot(process.env.MONGO_URL),
+    NotesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
